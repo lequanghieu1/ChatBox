936dafb3accf1565a26a26f4eba87c2e60212bf1
HieuLe112 <hieu.le@wiki-solution.com>
2022-11-17 21:58:01 +0700

    fix init sdk

--- src/views/oAuth.vue
+++ src/views/oAuth.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="w-full h-screen flex justify-center items-center">
-        <button class="font-bold bg-sky-600 p-3 rounded text-white h-16 w-40" @click="AppOauth">
-            Kích hoạt
-        </button>
-    </div>
+  <div class="w-full h-screen flex justify-center items-center">
+    <button
+      class="font-bold bg-sky-600 p-3 rounded text-white h-16 w-40"
+      @click="AppOauth"
+    >
+      Kích hoạt
+    </button>
+  </div>
 </template>
-  
 <script setup>
+import { initSdkApp } from "../common/sdkMethods.js";
+import { onMounted } from "vue";
+onMounted(async () => {
+  await initSdkApp();
+});
 const AppOauth = async () => {
-    await Sdk.doAppOAuth();
-    alert('Kích hoạt thành công')
-    setTimeout(() => window.close(), 1000)
+  await Sdk.doAppOAuth();
+  alert("Kích hoạt thành công");
+  setTimeout(() => window.close(), 1000);
 };
 </script>
-  
-<style scoped lang="scss">
-
-</style>
